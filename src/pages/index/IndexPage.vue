8029a5f52a5f7c871df9cd25eeb6e65c016a341d
fangjianchao <fangjianchao@lansea.com>
2019-10-12 11:20:56 +0800

    首页替换

---
+++ src/pages/index/IndexPage.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="IndexPage">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "IndexPage",
+        components: {},
+        props: {},
+        data() {
+            return {}
+        },
+        mounted() {
+        },
+        methods: {
+            reload() {
+            },
+        },
+        watch: {},
+        computed: {},
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+    .IndexPage {
+    }
+</style>
